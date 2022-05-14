345ec76fb77b7f2547dc0ec8416543dc5bd31eb8
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-05-14 20:44:33 +0700

    before footer done responsive

--- containers/Footer/BeforeFooterBighSight.jsx
+++ containers/Footer/BeforeFooterBighSight.jsx
@@ -8,47 +8,47 @@ export default function BeforeFooter() {
     return (
         <div>
             <div id={styles["BeforeFooter"]}>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Row>
+                <Row className={styles["BeforeFooter-Content-Container"]}>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={12}>
                                 <div className={styles["BeforeFooter-Logo"]}>
                                     <Image src={require("../../src/Big-Sight/LogoBigSight.png")} />
                                 </div>
-                            </Row>
-                            <Row>
+                            </Col>
+                            <Col md={12}>
                                 <h1 className={styles["BeforeFooter-Left-Title"]}>Telkom Direktorat Digital Bussiness Bandung</h1>
-                            </Row>
-                            <Row>
+                            </Col>
+                            <Col md={12} sm={6} xs={12}>
                                 <h1 className={styles["BeforeFooter-Left-Desc"]}>Jl. Gegerkalong Hilir no. 47, Sukasari, Kota Bandung, Jawa Barat 40152 (022) 4571261</h1>
-                            </Row>
-                            <Row>
+                            </Col>
+                            <Col md={12} sm={6} xs={12}>
                                 <h1 className={styles["BeforeFooter-Left-Desc"]}>admin@bigsight.id</h1>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row className={styles["BeforeFooter-Middle-Row1"]}></Row>
-                            <Row className={styles["BeforeFooter-Middle-Row2"]}></Row>
-                            <Row>
-                                <h1 className={styles["BeforeFooter-Middle-Title"]}>Ikuti Sosial Media Kami</h1>
-                                <div className={styles["BeforeFooter-Middle-Sosmed-Container"]}>
-                                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                                    <FontAwesomeIcon icon={faInstagram} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
-                                    <FontAwesomeIcon icon={faTwitter} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
-                                    <FontAwesomeIcon icon={faFacebookSquare} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
-                                </div>
-                            </Row>
-                            <Row>
-                                <h1 className={styles["BeforeFooter-Middle-Desc"]}>BIGSIGHT</h1>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <div className={styles["BeforeFooter-Right-Logo-Wrapper"]}>
-                                <Image src={require("../../src/Big-Sight/LogoRight.png")} />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <Row className={styles["BeforeFooter-Middle-Row1"]}></Row>
+                        <Row className={styles["BeforeFooter-Middle-Row2"]}></Row>
+                        <Row>
+                            <h1 className={styles["BeforeFooter-Middle-Title"]}>Ikuti Sosial Media Kami</h1>
+                            <div className={styles["BeforeFooter-Middle-Sosmed-Container"]}>
+                                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                                <FontAwesomeIcon icon={faInstagram} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
+                                <FontAwesomeIcon icon={faTwitter} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
+                                <FontAwesomeIcon icon={faFacebookSquare} size="2x" className={styles["BeforeFooter-Middle-Sosmed-Logo"]} />
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
+                        </Row>
+                        <Row>
+                            <h1 className={styles["BeforeFooter-Middle-Desc"]}>BIGSIGHT</h1>
+                        </Row>
+                    </Col>
+                    <Col md={4}>
+                        <div className={styles["BeforeFooter-Right-Logo-Wrapper"]}>
+                            <Image src={require("../../src/Big-Sight/LogoRight.png")} />
+                        </div>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
