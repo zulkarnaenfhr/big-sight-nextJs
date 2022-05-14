4a57996f9bcf2c4decbc993431d4453ab4e4c302
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-05-14 12:04:40 +0700

    contact us left column insyaallah bener

--- containers/ContactUs/ContactUs-Section1/ContactUs-Section1.jsx
+++ containers/ContactUs/ContactUs-Section1/ContactUs-Section1.jsx
@@ -16,24 +16,17 @@ class ContactUsSection1 extends Component {
             <div id={styles["ContactUs-Section1"]}>
                 <div className={styles["ContactUs-Section1-Form-Container"]}>
                     <Row>
-                        <Col sm={12} lg={4}>
-                            <div className={styles["ContactUs-Section1-Form-Content-Left-Container"]}>
-                                <div>
-                                    <div className={styles["ContactUs-Section1-Form-Content-Left-Perent-Text-Container"]}>
-                                        <div className={styles["ContactUs-Section1-Form-Content-Left-Text-Container"]}>
-                                            <h1 className={styles["ContactUs-Section1-Form-Content-Left-Title"]}>Contact our team</h1>
-                                            <p className={styles["ContactUs-Section1-Form-Content-Left-Desc"]}>We are happy to answer your question</p>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className={styles["ContactUs-Section1-Form-Content-Left-Background-Wrapper"]}>
-                                            <Image src={require("../../../src/Big-Sight/ContactUs/ContactUsBackground.png")} />
-                                        </div>
+                        <Col sm={12} lg={5} className={styles["ContactUs-Section1-Left-Column"]}>
+                            <div className={styles["ContactUs-Section1-Left-Parent-Content-Container"]}>
+                                <div className={styles["ContactUs-Section1-Left-Container"]}>
+                                    <div className={styles["ContactUs-Section1-Left-Content-Container"]}>
+                                        <h1 className={styles["ContactUs-Section1-Left-Title"]}>Contact our team</h1>
+                                        <h1 className={styles["ContactUs-Section1-Left-Desc"]}>We are happy to answer your question</h1>
                                     </div>
                                 </div>
                             </div>
                         </Col>
-                        <Col className={styles["ContactUs-Section1-Form-Content-Right-Container"]} sm={12} lg={8}>
+                        <Col className={styles["ContactUs-Section1-Form-Content-Right-Container"]} sm={12} lg={7}>
                             <h1>masuk</h1>
                         </Col>
                     </Row>
