52d666b44a3f1e5b3e441dd3aded5b333e2b9c2d
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-05-14 12:53:06 +0700

    contact us right draft 1

--- containers/ContactUs/ContactUs-Section1/ContactUs-Section1.jsx
+++ containers/ContactUs/ContactUs-Section1/ContactUs-Section1.jsx
@@ -27,7 +27,71 @@ class ContactUsSection1 extends Component {
                             </div>
                         </Col>
                         <Col className={styles["ContactUs-Section1-Form-Content-Right-Container"]} sm={12} lg={7}>
-                            <h1>masuk</h1>
+                            <form action="mailto:someone@example.com" method="post" encType="text/plain" className="BigSight-ContactUs-Section1-Form-Container">
+                                <Row>
+                                    <Col>
+                                        <label htmlFor="">First Name</label>
+                                        <br />
+                                        <input className="container" placeholder="First Name" type="text" name="First Name" />
+                                    </Col>
+                                    <Col>
+                                        <label htmlFor="">Last Name</label>
+                                        <br />
+                                        <input className="container" placeholder="Last Name" type="text" name="Last Name" />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <label htmlFor="">E-Mail</label>
+                                        <br />
+                                        <input className="container" placeholder="youremail@example.com" type="text" name="na1me" />
+                                    </Col>
+                                    <Col>
+                                        <label htmlFor="">Phone</label>
+                                        <br />
+                                        <input className="container" placeholder="08123456789101" type="text" name="Email" />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <p>What the of product do you need</p>
+                                    <Row>
+                                        <div className={styles["ContactUs-Section1-Right-Radio-Row"]}>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Legal Analytics"} id="" /> Legal Analytics
+                                            </label>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Big View"} id="" /> Big View
+                                            </label>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Picaso"} id="" /> Picaso
+                                            </label>
+                                        </div>
+                                    </Row>
+                                    <Row>
+                                        <div className={styles["ContactUs-Section1-Right-Radio-Row"]}>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Smarteye.id"} id="" /> Smarteye.id
+                                            </label>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Big Score"} id="" /> Big Score
+                                            </label>
+                                            <label htmlFor="" className={styles["ContactUs-Section1-Right-Radio-Row-Product"]}>
+                                                {" "}
+                                                <input type="radio" name="Produk Interest" value={"Big Box"} id="" /> Big Box
+                                            </label>
+                                        </div>
+                                    </Row>
+                                </Row>
+                                <label htmlFor="">Message</label>
+                                <br />
+                                <textarea className="container" placeholder="Message" type="text" name="Phone Number" cols="30" rows="10" />
+                                <button type="submit">Submit</button>
+                            </form>{" "}
                         </Col>
                     </Row>
                 </div>
