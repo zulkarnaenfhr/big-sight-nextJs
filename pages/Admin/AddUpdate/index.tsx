5ed93c0133a87a1aca0b800d1edfbfad9c129198
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-05-17 12:15:02 +0700

    folder structure admin

---
+++ pages/Admin/AddUpdate/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddUpdate() {
+    return (
+        <div>
+            <h1>masuk</h1>
+        </div>
+    );
+}
---
+++ pages/Admin/BigSightUpdate/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BigSightUpdate() {
+    return (
+        <div>
+            <h1>masuk landing page berita</h1>
+        </div>
+    );
+}
